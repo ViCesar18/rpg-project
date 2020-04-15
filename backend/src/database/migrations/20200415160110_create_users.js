89bd76ffe2fb31d6fbd5e32563afdb923917a92b
ViCesar18 <viniciuscesar_cesar@hotmail.com>
2020-04-15 16:32:07 -0300

    Iniciei backend do app, criei tabela para usuários

---
+++ backend/src/database/migrations/20200415160110_create_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function (table) {
+      table.increments('userId')
+      table.string('username', 16).notNullable()
+      table.string('password', 16).notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
