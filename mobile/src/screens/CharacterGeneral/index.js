139cff849372fd466661711d6184ac86306b17a5
ViCesar18 <viniciuscesar_cesar@hotmail.com>
2020-05-19 19:10:32 -0300

    primeiro protótipo do header

--- mobile/src/screens/CharacterGeneral/index.js
+++ mobile/src/screens/CharacterGeneral/index.js
@@ -1,12 +1,119 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, ScrollView, TouchableHighlight, Image } from 'react-native'
+import { CheckBox } from 'native-base'
+
+import { Feather } from '@expo/vector-icons'
 
 import styles from './styles'
 
-export default function CharacterGeneral() {
+export default function CharacterGeneral({ navigation }) {
     return (
         <View style={styles.container}>
-            <Text>Tela de Personagem (Geral)</Text>
+            <View style={styles.header}>
+                <TouchableHighlight
+                    onPress={navigation.openDrawer}
+                    underlayColor="transparent"
+                >
+                    <Feather name={'menu'} size={36} color={'#FFF'} />
+                </TouchableHighlight>
+
+                <TextInput style={styles.characterName} placeholder="Nome do Personagem"  value="Edran Galanodel" />
+
+                <View style={styles.headerInputs1}>
+                    <TextInput style={styles.headerInput} placeholder="Raça" />
+                    <TextInput style={styles.headerInput} placeholder="Classe" />
+                    <TextInput style={styles.headerInput} placeholder="Antecedente" />
+                </View>
+                    
+                <View style={styles.headerInputs2}>
+                    <TextInput style={styles.headerInput} placeholder="XP" />
+                    
+                    <Image style={styles.headerImage} source={require('../../assets/avatar/elf.png')} />
+                    <TextInput style={styles.headerLevel} placeholder="Nível" />
+
+                    <TextInput style={styles.headerInput} placeholder="Tendência" />
+                </View>
+            </View>
+
+            <ScrollView style={styles.content}>
+                <View style={styles.inputsContainer1}>
+                    <View style={styles.rectInputContainer}>
+                        <TextInput style={styles.rectInput} />
+                        <Text style={styles.rectInputText}>Classe de Armadura</Text>
+                    </View>
+
+                    <View style={styles.rectInputContainer}>
+                        <TextInput style={styles.rectInput} />
+                        <Text style={styles.rectInputText}>Iniciativa</Text>
+                    </View>
+
+                    <View style={styles.rectInputContainer}>
+                        <TextInput style={styles.rectInput} />
+                        <Text style={styles.rectInputText}>Deslocamento</Text>
+                    </View>
+                </View>
+
+                <View style={styles.hitPointsContainer}>
+                    <View style={styles.hitPoints}>
+                        <Text style={styles.hitPointsText}>PV Total</Text>
+                        <Feather name={'heart'} size={36} color={'red'} />
+                    </View>
+                    <View style={styles.temporaryHitPoints}>
+                        <Text style={styles.temporaryHitPointsText}>PV Temporário</Text>
+                        <Feather name={'heart'} size={36} color={'yellow'} />
+                    </View>
+                </View>
+
+                <View style={styles.inputsContainer2}>
+                    <View style={styles.deathSavesContainer}>
+                        <View style={styles.savesContainer}>
+                            <Text>Sucesso</Text>
+                            <CheckBox />
+                            <CheckBox />
+                            <CheckBox />
+                        </View>
+
+                        <View style={styles.savesContainer}>
+                            <Text>Falha</Text>
+                            <CheckBox />
+                            <CheckBox />
+                            <CheckBox />
+                        </View>
+
+                        <Text>TESTE CONTRA A MORTE</Text>
+                    </View>
+
+                    <View styles={styles.hpDicesContainer}>
+                        <View>
+                            <Text>Total de Dados</Text>
+                            <TextInput />
+                        </View>
+                        <TextInput />
+                        <Text>DADOS DE VIDA</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <View>
+                            <Text>Nome</Text>
+                            <TextInput />
+                            <TextInput />
+                        </View>
+                        <View>
+                            <Text>Bônus</Text>
+                            <TextInput />
+                            <TextInput />
+                        </View>
+                        <View>
+                            <Text>Dano/Tipo</Text>
+                            <TextInput />
+                            <TextInput />
+                        </View>
+                    </View>
+                    <TextInput />
+                </View>
+            </ScrollView>
         </View>
     )
 }
--- mobile/src/screens/CharacterGeneral/styles.js
+++ mobile/src/screens/CharacterGeneral/styles.js
@@ -1,9 +1,80 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
+import Constants from 'expo-constants'
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#E3E8E8',
+    },
+
+    header: {
+        backgroundColor: '#353535',
+        paddingTop: Constants.statusBarHeight + 10,
+        paddingHorizontal: 15,
+        borderBottomStartRadius: 40,
+        borderBottomEndRadius: 40,
+        elevation: 10
+    },
+
+    headerButtonContainer: {
+        flexDirection: 'row',
+    },
+
+    characterName: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center', 
+    },
+
+    headerInput: {
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        height: 40,
+        width: '30%',
+        textAlign: 'center',
+        fontSize: 16
+    },
+
+    headerInputs1: {
+        paddingTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    headerInputs2: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 20,
+        paddingBottom: 30,
+    },
+
+    headerImage: {
+        position: 'absolute',
+        left: '34.5%',
+        top: '20%',
+
+        width: 120,
+        height: 120,
+        backgroundColor: '#E3E8E8',
+        borderRadius: 8,
+        borderColor: '#353535',
+        borderWidth: 3,
+    },
+
+    headerLevel: {
+        position: 'absolute',
+        left: '25%',
+        top: '160%',
+
+        width: 50,
+        height: 50,
+        backgroundColor: '#353535',
+        borderRadius: (Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        borderColor: '#E3E8E8',
+        borderWidth: 3,
+        fontSize: 16,
+        textAlign: 'center',
     }
 })
