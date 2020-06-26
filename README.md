4546509dd68b5815feb47411c4142d49d5b97f71
unknown <giacomomagri@hotmail.com>
2020-06-25 23:47:12 -0300

    alterei o readme

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Aplicação para auxiliar os jogadores de RPG na organização de suas partidas.
     npm run start
 
 ## Para subir o mobile:
+    alterar o arquivo /mobile/src/services/api.js setando baseURL com o valor https://SEU_IP:3333/api
     expo start
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const ip = axios.create({baseURL: 'https://api.ipify.org?format=json'})
+
+console.log(ip.ip)
+
 const api = axios.create({
-    baseURL: 'http://192.168.0.106:3333/api'
+    baseURL: 'http://192.168.0.38:3333/api'
 })
 
 export default api
